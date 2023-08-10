172a5b4e83cffaad528bc26cc181061071bf041d
KortayLuna <kortaykokturk@gmail.com>
2023-08-10 15:13:15 +0300

    Finished The Project

---
+++ generator.py
@@ -0,0 +1,22 @@
+import json
+from random import choice
+
+char = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!'^^+%&/()=?_-.,"
+
+length = int(input("Please Enter The Lenght You Want For Your Password To Be: "))
+whereToUse = str(input("Where Are You Going To Use This Password?: "))
+passwords= []
+with open("store_password.json", "r") as f:
+    storedpasswords = json.load(f)
+
+password = ''
+for len in range(length):
+    password += choice(char)
+
+passwords = {"WhereToUse": whereToUse, "Password": password}
+storedpasswords["passwords"].append(passwords)
+
+
+with open("store_password.json", "w") as f:
+    json.dump(storedpasswords, f, indent=2)
+
